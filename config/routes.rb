f08cdb9f5911ae35a6040bfbf54b57db5342e47d
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-05-08 09:46:03 +0000

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
+    post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
+  end
   resources :books
   get 'home/about' => 'homes#about'
-  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
-  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
+
 end
